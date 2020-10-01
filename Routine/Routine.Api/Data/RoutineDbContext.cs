d55ef0ac374a2d0f93d7f0d5aa351bb8303caac1
ruitengsoftware <3244106825@qq.com>
2020-10-01 16:52:05 +0800

    2020nian 10yue 1hao

---
+++ Routine/Routine.Api/Data/RoutineDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Routine.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routine.Api.Data
+{
+    public class RoutineDbContext:DbContext
+    {
+        public RoutineDbContext(DbContextOptions<RoutineDbContext> options):base(options)
+        { 
+        
+        
+        
+        }
+        public DbSet<Company> Companies { get; set; }
+
+        public DbSet<Employee> Employees { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Company>().Property(x => x.Name).IsRequired().HasMaxLength(100);
+        }
+    }
+}
